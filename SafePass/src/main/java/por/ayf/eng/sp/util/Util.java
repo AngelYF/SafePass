4b4a73e9a0fdb090a48d4b62009eaa8abf10dfec
Ángel Yagüe <angelya91@gmail.com>
2020-12-07 23:55:22 +0100

    1.0.8 - Se crea clase Util y nuevo método para visualizar mensajes.

---
+++ SafePass/src/main/java/por/ayf/eng/sp/util/Util.java
@@ -0,0 +1,50 @@
+package por.ayf.eng.sp.util;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.JOptionPane;
+
+/**
+ *  Class Util with some util methods.
+ * 
+ *  @author: Ángel Yagüe Flor.
+ *  @version: 1.0.
+ */
+
+public class Util {
+	
+	public static void logMessage(String nameClass, String message, int typeMessage, Exception e) {
+		
+		switch(typeMessage) {
+			case 0:	// ERROR
+				Logger.getLogger(nameClass).log(Level.SEVERE, message, e);
+				
+				JOptionPane.showMessageDialog(null,
+						message,
+						"Error", 
+						JOptionPane.ERROR_MESSAGE);
+				break;
+			case 1:	// INFORMATION
+				JOptionPane.showMessageDialog(null,
+						message,
+						"Información", 
+						JOptionPane.INFORMATION_MESSAGE);
+				break;
+			case 2:	// WARNING
+				JOptionPane.showMessageDialog(null,
+						message,
+						"Advertencia", 
+						JOptionPane.WARNING_MESSAGE);
+			case 3:	// QUESTION
+				JOptionPane.showMessageDialog(null,
+						message,
+						"Pregunta", 
+						JOptionPane.QUESTION_MESSAGE);
+				break;
+			default:
+				break;
+		}
+		
+	}
+}
